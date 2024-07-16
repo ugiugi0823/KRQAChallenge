59078015ab6d353fd5c1d9fed723606a5ad3d811
ugiugi0823 <acerghjk@gmail.com>
2024-07-16 15:53:58 +0900

    Initial commit

---
+++ run.sh
@@ -0,0 +1,2 @@
+export WANDB_DEBUG=true
+CUDA_VISIBLE_DEVICES=0 train.py --train_path ./data/train.csv
---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+start_time=$(date +%s)
+
+CUDA_VISIBLE_DEVICES=0 python test.py \
+--model_path /data2/hyunwook/dacon/ckpt/yanolja/EEVE-Korean-Instruct-10.8B-v1.0/20240716_084420/checkpoint-50
+
+end_time=$(date +%s)
+elapsed_time=$((end_time - start_time))
+
+echo "Elapsed time: ${elapsed_time} seconds"
